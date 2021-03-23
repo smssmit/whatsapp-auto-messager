4bc27049c1f8bd2aceda67261cba21847a2f7af0
smssmit <56910234+smssmit@users.noreply.github.com>
2021-03-23 23:16:08 +0530

    Add files via upload

---
+++ Whatsapp.py
@@ -0,0 +1,5 @@
+import pywhatkit as kit #installpywhatkit
+import os
+kit.sendwhatmsg("Enter your phone no. and add country code","Enter Your Message",24,00) #at the end enter time in 24 hours format
+os.system("taskkill /im chrome.exe /f") #it will close the browser
+os.system("shutdown /s /t 1") #it will shutdown the pc
